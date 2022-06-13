400ee9b8e562cc19abe06078799f28f924ef47af
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-06-13 18:21:07 +0100

    Update main.js file with the ability to change icons to X and back to bars

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const body = document.querySelectorAll('.intro-card,.intro-cards');
+
 const mobileMenu = () => {
     hamburger.classList.toggle("active");
+    hamburger.innerHTML = "<i class='fas fa-times'></i>";
     navMenu.classList.toggle("active");
-    body.forEach(n=> n.classList.toggle("bgcolor"))
-}
+    body.forEach(n=> n.classList.toggle("bgcolor"));
 
-hamburger.addEventListener("click", mobileMenu);
+}
 
 const closeMenu = () => {
     hamburger.classList.remove("active");
+    hamburger.innerHTML = "<i class='fas fa-bars'></i>";
     navMenu.classList.remove("active");
     body.forEach(n=> n.classList.remove("bgcolor"))
 }
+
 const navLink = document.querySelectorAll(".nav-link");
 navLink.forEach( n => n.addEventListener("click", closeMenu));
 
+hamburger.addEventListener("click", mobileMenu, false);
 
 
