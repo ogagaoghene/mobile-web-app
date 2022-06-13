5761efc957bedfe5b2837c3fa2a99605cc0ae42a
Hamma Azarok <hammaagazarok@gmail.com>
2022-06-13 17:39:36 +0200

    edit navbar

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
-
+const body = document.querySelectorAll('.intro-card,.intro-cards');
 const mobileMenu = () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    body.forEach(n=> n.classList.toggle("bgcolor"))
 }
 
 hamburger.addEventListener("click", mobileMenu);
 
-const navLink = document.querySelectorAll(".nav-link");
-navLink.forEach( n => n.addEventListener("click", closeMenu));
-
 const closeMenu = () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
+    body.forEach(n=> n.classList.remove("bgcolor"))
 }
+const navLink = document.querySelectorAll(".nav-link");
+navLink.forEach( n => n.addEventListener("click", closeMenu));
+
+
 
