ef67c66816c568797f890f0d4dd7a04b18bd8d6a
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-06-16 08:35:02 +0100

    Add index.html file, css file and Javascript file

--- modal-window.js
+++ modal-window.js
@@ -1,7 +1,6 @@
-const closeModalButtons = document.querySelectorAll('.button-styles-2');
-const cards_container = document.getElementById("work-section");
+const cardsContainer = document.getElementById('work-section');
 
-let cards = [
+const cards = [
   {
     img: 'assets/1.png',
     project_title: 'TONIC',
@@ -9,7 +8,7 @@ let cards = [
     dev_title: 'BackEndDev',
     date: '2015',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    skills: ['html', 'Ruby on rails', 'css', 'Javascript']
+    skills: ['html', 'Ruby on rails', 'css', 'Javascript'],
   },
   {
     img: 'assets/2.png',
@@ -18,9 +17,8 @@ let cards = [
     dev_title: 'BackEndDev',
     date: '2015',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    skills: ['html', 'Ruby on rails', 'css', 'Javascript']
-  }
-  ,
+    skills: ['html', 'Ruby on rails', 'css', 'Javascript'],
+  },
   {
     img: 'assets/3.png',
     project_title: 'TONIC',
@@ -28,9 +26,8 @@ let cards = [
     dev_title: 'BackEndDev',
     date: '2015',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    skills: ['html', 'Ruby on rails', 'css', 'Javascript']
-  }
-  ,
+    skills: ['html', 'Ruby on rails', 'css', 'Javascript'],
+  },
   {
     img: 'assets/4.png',
     project_title: 'TONIC',
@@ -38,110 +35,107 @@ let cards = [
     dev_title: 'BackEndDev',
     date: '2015',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    skills: ['html', 'Ruby on rails', 'css', 'Javascript']
-  }
+    skills: ['html', 'Ruby on rails', 'css', 'Javascript'],
+  },
 ];
 
 function loadCards() {
-  let card_html = ''; /** created an empty string as placeholder */
+  let cardHtml = '';
 
-  for (let card_index = 0; card_index < cards.length; card_index++) {
-    let html = `<div class="work-section-card">
-  <div class="flex-left">
-    <img src="${cards[card_index].img}" alt="Nature image" />
-  </div>
-  <div class="flex-right">
-    <h2>${cards[card_index].project_title}</h2>
-    <div class="break">&nbsp;</div>
-    <span>
-    <strong>${cards[card_index].company} 
-    </strong>&nbsp; </span> &nbsp;  <img src="assets/dot.png" alt="" /><span>&nbsp; ${cards[card_index].dev_title} &nbsp; </span><img src="assets/dot.png" alt="" />&nbsp; <span>${cards[card_index].date}</span>
-    <p>
-     ${cards[card_index].description}
-    </p>
-    <ul>
-      <li><a href="#" class="anchorstyles">${cards[card_index].skills[0]}</a></li>
-      <li><a href="#" class="anchorstyles">${cards[card_index].skills[1]}</a></li>
-      <li><a href="#" class="anchorstyles">${cards[card_index].skills[2]}</a></li>
-      <li><a href="#" class="anchorstyles">${cards[card_index].skills[3]}</a></li>
-    </ul>
-    <div class="break"></div>
-    
+  for (let cardIndex = 0; cardIndex < cards.length; cardIndex += 1) {
+    const html = `<div class="row">
+
+    <div class="left_div">
+      <img src="${cards[cardIndex].img}" alt="Nature image" />
+    </div>
+
+    <div class="right_div">
+      <h2>${cards[cardIndex].project_title}</h2>
+      <div class="break">&nbsp;</div>
+      <span>
+      <strong>
+        ${cards[cardIndex].company} 
+      </strong>&nbsp; </span> &nbsp;  <img src="assets/dot.png" alt="" /><span>&nbsp; ${cards[cardIndex].dev_title} &nbsp; </span><img src="assets/dot.png" alt="" />&nbsp; <span>${cards[cardIndex].date}</span>
+      <p>${cards[cardIndex].description}</p>
+      <ul>
+        <li><a href="#" class="anchorstyles">${cards[cardIndex].skills[0]}</a></li>
+        <li><a href="#" class="anchorstyles">${cards[cardIndex].skills[1]}</a></li>
+        <li><a href="#" class="anchorstyles">${cards[cardIndex].skills[2]}</a></li>
+        <li><a href="#" class="anchorstyles">${cards[cardIndex].skills[3]}</a></li>
+      </ul>
+      <div class="break"></div>
+      
     <div>
       <button id="button-styles" class="button-styles">See Projects</button>
     </div>
 
   </div>
-</div>`
-    card_html+=html;
+</div>`;
+    cardHtml += html;
   }
-  cards_container.innerHTML = card_html;
+  cardsContainer.innerHTML = cardHtml;
 }
 
 loadCards();
 
-function loadModal(id){
-  id = 0;
-  let project = cards[id];
-  //let moodal = '';
-  let modal_html = `
-  <div class="modal-header">
-  <div class="work-section-card">
-  <div class="modal-head">
-    <div class="title">${project.project_title}</div>
-    <i class="fas fa-times close-button"></i>
-    </div>
-    <div class="flex-left">
-    <div>
-      <span>
-        <strong>${project.company}
-        </strong>&nbsp; </span> &nbsp; <img src="assets/dot.png" alt="" /><span>&nbsp; ${project.dev_title} &nbsp; </span><img
-        src="assets/dot.png" alt="" />&nbsp; ${project.date}<span></span>
-        </div>
-      <img src="${project.img}" alt="Nature image" />
-    </div>
-      <div class="dec-container">
-      <div class="desc">
-      <p>
-        ${project.description}
-      </p>
+function loadModal(id) {
+  id = 2;
+  const project = cards[id];
+
+  const modalHtml = `
+  <div classs="wrapper">
+    <div class="modal-body">
+    <div class="title">${project.project_title} <i class="fas fa-times close-button"></i></div>
+      
+    <span><strong>${project.company}</strong>&nbsp;</span> &nbsp; <img src="assets/dot.png" alt="dot icon" /> &nbsp; <span>&nbsp; ${project.dev_title} &nbsp; </span>
+    <img src="assets/dot.png" alt="" />&nbsp; ${project.date}<span></span>
+      <div>
+        <img src="${project.img}" alt="Nature image" />
+        <p>${project.description}</p>
       </div>
-      <div class="skills">
-      <ul class="frame">
-        <li><a href="#" class="anchorstyles">html</a></li>
-        <li><a href="#" class="anchorstyles">CSS</a></li>
-        <li><a href="#" class="anchorstyles">JavaScript</a></li>
-      </ul>
-      <div class="break"></div>
-      <div class="button-frame">
-        <button class="button-styles">See Live <i class="fa-regular fa-circle-up"></i></button>
-        <button class="button-styles">See Source <i class="fab fa-github"></i></button>
+
+      <div>
+        <ul>
+          <li><a href="#" class="anchorstyles">html</a></li>
+          <li><a href="#" class="anchorstyles">css</a></li>
+          <li><a href="#" class="anchorstyles">javascript</a></li>
+        </ul>
+
+        <div class="button-frame">
+          <button class="button-styles">See Live <i class="fa-regular fa-circle-up"></i></button>
+          <button class="button-styles">See Source <i class="fab fa-github"></i></button>
+       </div>
       </div>
+      
+
+     
+
     </div>
-    </div>
-  </div>`
-let body_container = document.getElementById("modal");
-body_container.innerHTML = modal_html;
+
+  </div>`;
+  const bodyContainer = document.getElementById('modal');
+  bodyContainer.innerHTML = modalHtml;
 }
+
 loadModal();
 
-const modal = document.querySelector(".modal");
-const trigger = document.querySelectorAll(".button-styles");
-const closeButton = document.querySelector(".close-button");
+const modal = document.querySelector('.modal');
+const trigger = document.querySelectorAll('.button-styles');
+const closeButton = document.querySelector('.close-button');
 
 function toggleModal() {
-    modal.classList.toggle("show-modal");
+  modal.classList.toggle('show-modal');
 }
 
 function windowOnClick(event) {
-    if (event.target === modal) {
-        toggleModal();
-    }
+  if (event.target === modal) {
+    toggleModal();
+  }
 }
 
-trigger.forEach(element => {
-  element.addEventListener("click", toggleModal);
+trigger.forEach((element) => {
+  element.addEventListener('click', toggleModal);
 });
 
-closeButton.addEventListener("click", toggleModal);
-window.addEventListener("click", windowOnClick);
+closeButton.addEventListener('click', toggleModal);
+window.addEventListener('click', windowOnClick);
